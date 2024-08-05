6c7c9bd8532427f1a0ac998676a4436e5d59dbda
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-08-05 05:39:09 +0200

    UI für Currency Master hinzugefügt

--- CurrencyConverter_static/MainWindow.xaml.cs
+++ CurrencyConverter_static/MainWindow.xaml.cs
@@ -107,5 +107,20 @@ namespace CurrencyConverter_static
 			Regex regex = new Regex("[^0-9]+");
 			e.Handled = regex.IsMatch(e.Text);
 		}
+
+		private void btnSave_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnCancel_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void dgvCurrency_SelectedCellsChanged(object sender, System.Windows.Controls.SelectedCellsChangedEventArgs e)
+		{
+
+		}
 	}
 }
